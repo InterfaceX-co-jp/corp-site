e57d2e09c9a956bc28a3564696af133e9cc32d96
mikana0918 <shogo.kusuhara@gmail.com>
2021-06-26 17:47:02 +0900

    feat: :sparkles: add window measure plugin

---
+++ plugins/window.ts
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import { Plugin } from '@nuxt/types'
+
+export interface window {
+  pageYOffset: number
+  width: number
+  height: number
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $window: window
+  }
+}
+
+declare module '@nuxt/types' {
+  interface NuxtAppOptions {
+    $window: window
+  }
+  interface Context {
+    $window: window
+  }
+}
+
+const $window = Vue.observable({
+  width: 0,
+  height: 0,
+  pageYOffset: 0,
+})
+
+// SSR 時にエラーが出るため process.browser で分岐
+// Nuxt を使用しなければこの分岐は削除してください
+if (process.browser) {
+  const onScroll = () => {
+    $window.pageYOffset = global.pageYOffset
+  }
+  const onResize = () => {
+    $window.width = document.documentElement.clientWidth
+    $window.height = global.innerHeight
+  }
+  global.addEventListener('scroll', onScroll)
+  global.addEventListener('resize', onResize)
+  // 一度だけスクロールハンドラとリサイズハンドラを直接呼んで初期値をセット
+  onScroll()
+  onResize()
+}
+
+const plugin: Plugin = (_, inject) => {
+  inject('window', $window)
+}
+
+export default plugin
